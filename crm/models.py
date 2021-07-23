e64bda30d192a6decbc02747eefa671b9415d514
Alexander <alexmail.dev@gmail.com>
2021-07-23 10:10:05 +0900

    Add field insta

--- crm/models.py
+++ crm/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
+    insta = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
